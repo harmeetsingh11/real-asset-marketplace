bec58a5742586a198cb1d4d0b1643ccf51f39599
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-07-28 04:41:53 +0530

    feat: DatabaseService class extends PrismaClient inherits methods(init,destroy) for interacting with the database

--- backend/src/database/database.service.ts
+++ backend/src/database/database.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class DatabaseService {}
+export class DatabaseService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
