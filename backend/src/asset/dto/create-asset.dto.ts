d166b9d571dfbbb498481bb599d8a71314d438f0
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-07-30 04:58:45 +0530

    feat: create new asset dto

--- backend/src/asset/dto/create-asset.dto.ts
+++ backend/src/asset/dto/create-asset.dto.ts
@@ -1,1 +1,22 @@
-export class CreateAssetDto {}
+import { IsString, IsNumber, IsArray, ArrayNotEmpty } from 'class-validator';
+
+export class CreateAssetDto {
+  @IsNumber()
+  userId: number;
+
+  @IsString()
+  assetName: string;
+
+  @IsString()
+  description: string;
+
+  @IsNumber()
+  price: number;
+
+  @IsString()
+  category: string;
+
+  @IsArray()
+  @ArrayNotEmpty()
+  images: string[];
+}
