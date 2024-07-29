8fccdc83aa678a38199cb1da55cab41ea518d9d8
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-07-30 02:35:05 +0530

    feat: implement JWT strategy to validate user with email

---
+++ backend/src/auth/jwt.strategy.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { DatabaseService } from 'src/database/database.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly databaseService: DatabaseService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: { email: string }) {
+    const user = await this.databaseService.user.findUnique({
+      where: {
+        email: payload.email,
+      },
+    });
+    return user;
+  }
+}
