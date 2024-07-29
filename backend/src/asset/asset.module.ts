4b104357ad03d3583f5e5cf8c6dffd6337e11b87
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-07-29 21:13:54 +0530

    chore: import database module

--- backend/src/asset/asset.module.ts
+++ backend/src/asset/asset.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AssetService } from './asset.service';
 import { AssetController } from './asset.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [AssetController],
   providers: [AssetService],
 })
