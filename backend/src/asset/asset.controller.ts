1fa27c2d4d426c81105762e0fc984b65fe1f8009
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-07-29 20:52:37 +0530

    feat: generate asset resource(module, controller and service) with CRUD endpoints using nest-cli

---
+++ backend/src/asset/asset.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { AssetService } from './asset.service';
+import { CreateAssetDto } from './dto/create-asset.dto';
+import { UpdateAssetDto } from './dto/update-asset.dto';
+
+@Controller('asset')
+export class AssetController {
+  constructor(private readonly assetService: AssetService) {}
+
+  @Post()
+  create(@Body() createAssetDto: CreateAssetDto) {
+    return this.assetService.create(createAssetDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.assetService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.assetService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateAssetDto: UpdateAssetDto) {
+    return this.assetService.update(+id, updateAssetDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.assetService.remove(+id);
+  }
+}
