182c42f5e508f00dbd3173eb275f9c1723a7d185
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-07-29 20:53:16 +0530

    chore: import asset module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
+import { AssetModule } from './asset/asset.module';
 
 @Module({
-  imports: [DatabaseModule, AuthModule],
+  imports: [DatabaseModule, AuthModule, AssetModule],
   controllers: [AppController],
   providers: [AppService],
 })
