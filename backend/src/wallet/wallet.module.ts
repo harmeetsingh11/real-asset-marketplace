ab6c403e1fd1035f077a91e8f5cba212ec17550a
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-07-30 05:05:57 +0530

    feat: import Database module in wallet module

--- backend/src/wallet/wallet.module.ts
+++ backend/src/wallet/wallet.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WalletService } from './wallet.service';
 import { WalletController } from './wallet.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [WalletController],
   providers: [WalletService],
 })
