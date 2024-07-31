91daa76ff3cd67bad2ad4b7295b3cdf2fa3afa03
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-07-31 19:11:43 +0530

    chore: import the DatabaseModule in CartModule

--- backend/src/cart/cart.module.ts
+++ backend/src/cart/cart.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CartService } from './cart.service';
 import { CartController } from './cart.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [CartController],
   providers: [CartService],
 })
