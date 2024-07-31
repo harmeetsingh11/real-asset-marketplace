ad0f01b5b86037fbca04e67ad4664dd3dc6e4af2
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-08-01 02:46:15 +0530

    chore: import database module in user module

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [UserController],
   providers: [UserService],
 })
