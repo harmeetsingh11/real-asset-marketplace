a6fdabea3b0c6362db16e5ad13af96dc042c69a2
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-08-01 05:18:11 +0530

    feat: add JWT Auth Guard at each cart api endpoint (add, view, checkout)

--- backend/src/cart/cart.controller.ts
+++ backend/src/cart/cart.controller.ts
@@ -1,22 +1,26 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
 import { CartService } from './cart.service';
 import { CreateCartItemDto } from './dto/create-cart.dto';
 import { CheckoutCartDto } from './dto/checkout-cart.dto';
+import { JwtAuthGuard } from 'src/auth/auth.guard';
 
 @Controller('api/cart')
 export class CartController {
   constructor(private readonly cartService: CartService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post('add')
   async addToCart(@Body() createCartItemDto: CreateCartItemDto) {
     return this.cartService.addToCart(createCartItemDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get('view')
   async viewCart(@Query('userId') userId: number) {
     return this.cartService.viewCart(userId);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post('checkout')
   async checkoutCart(@Body() checkoutCartDto: CheckoutCartDto) {
     return this.cartService.checkoutCart(checkoutCartDto);
