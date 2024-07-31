e45f2a3a7e11a7ca9642af1c25be6ab453f36fe3
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-08-01 05:18:49 +0530

    feat: add JWT Auth Guard at each wallet api endpoint (connect)

--- backend/src/wallet/wallet.controller.ts
+++ backend/src/wallet/wallet.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { WalletService } from './wallet.service';
 import { ConnectWalletDto } from './dto/connect-wallet.dto';
+import { JwtAuthGuard } from 'src/auth/auth.guard';
 
 @Controller('api/wallet')
 export class WalletController {
   constructor(private walletService: WalletService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post('connect')
   async connectWallet(@Body() connectWalletDto: ConnectWalletDto) {
     return this.walletService.connectWallet(connectWalletDto);
