48c2cee069d7e13ed58fcb4c6efe312f0c1ada07
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-08-01 05:10:38 +0530

    feat: import token blacklist module in user module

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { DatabaseModule } from 'src/database/database.module';
+import { TokenBlacklistModule } from 'src/token-blacklist/token-blacklist.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, TokenBlacklistModule],
   controllers: [UserController],
   providers: [UserService],
 })
