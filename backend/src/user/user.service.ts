76fb7f32ce669d3e37c3bf7ecc53edef76bdfd59
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-08-01 05:12:04 +0530

    feat: implement business for user logout api endpoint by blacklisting the token

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -4,12 +4,16 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 import { UserProfileDto } from './dto/user-profile.dto';
-// import { UserLogoutDto } from './dto/user-logout.dto';
 import { DatabaseService } from 'src/database/database.service';
+import { UserLogoutDto } from './dto/user-logout.dto';
+import { TokenBlacklistService } from 'src/token-blacklist/token-blacklist.service';
 
 @Injectable()
 export class UserService {
-  constructor(private dataservice: DatabaseService) {}
+  constructor(
+    private dataservice: DatabaseService,
+    private tokenBlacklistService: TokenBlacklistService,
+  ) {}
 
   async getUserProfile(userProfileDto: UserProfileDto) {
     let { userId } = userProfileDto;
@@ -37,5 +41,23 @@ export class UserService {
       userId: user.id,
       email: user.email,
     };
+  }
+
+  async logoutUser(userLogoutDto: UserLogoutDto) {
+    const { userId, token } = userLogoutDto;
+
+    // Validate user existence
+    const user = await this.dataservice.user.findUnique({
+      where: { id: userId },
+    });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    // Invalidate the token
+    await this.tokenBlacklistService.addTokenToBlacklist(token);
+
+    return { status: 'User logged out successfully' };
   }
 }
