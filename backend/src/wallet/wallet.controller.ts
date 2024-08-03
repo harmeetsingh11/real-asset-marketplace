1672b81c9f1b8cc865af2ac6736230ad4d381e29
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-08-04 04:38:03 +0530

    feat: add API tags, API operations with description, summary at wallet connect api endpoints

--- backend/src/wallet/wallet.controller.ts
+++ backend/src/wallet/wallet.controller.ts
@@ -2,13 +2,19 @@ import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { WalletService } from './wallet.service';
 import { ConnectWalletDto } from './dto/connect-wallet.dto';
 import { JwtAuthGuard } from 'src/auth/auth.guard';
-
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+@ApiTags('Wallet')
 @Controller('wallet')
 export class WalletController {
   constructor(private walletService: WalletService) {}
 
   @UseGuards(JwtAuthGuard)
   @Post('connect')
+  @ApiOperation({
+    description:
+      "This endpoint connects a user's blockchain wallet to their account by accepting the user's ID and wallet address, and returns a wallet ID and connection status.",
+    summary: 'Connect a blockchain wallet',
+  })
   async connectWallet(@Body() connectWalletDto: ConnectWalletDto) {
     return this.walletService.connectWallet(connectWalletDto);
   }
