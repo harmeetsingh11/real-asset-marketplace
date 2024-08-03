8466d02cba786200068c6564cca1efae27465c35
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-08-04 04:04:37 +0530

    feat: add @ApiProperty() to create asset data transfer object (dto)

--- backend/src/asset/dto/create-asset.dto.ts
+++ backend/src/asset/dto/create-asset.dto.ts
@@ -1,21 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNumber, IsArray, ArrayNotEmpty } from 'class-validator';
 
 export class CreateAssetDto {
+  @ApiProperty()
   @IsNumber()
   userId: number;
 
+  @ApiProperty()
   @IsString()
   assetName: string;
 
+  @ApiProperty()
   @IsString()
   description: string;
 
+  @ApiProperty()
   @IsNumber()
   price: number;
 
+  @ApiProperty()
   @IsString()
   category: string;
 
+  @ApiProperty()
   @IsArray()
   @ArrayNotEmpty()
   images: string[];
