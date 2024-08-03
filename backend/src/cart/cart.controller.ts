b64baa970aecd7f98394c7d404d0967d91530391
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-08-04 04:24:26 +0530

    feat: add API tags, API operations with description, summary at add, view and checkout cart api endpoints

--- backend/src/cart/cart.controller.ts
+++ backend/src/cart/cart.controller.ts
@@ -3,25 +3,41 @@ import { CartService } from './cart.service';
 import { CreateCartItemDto } from './dto/create-cart.dto';
 import { CheckoutCartDto } from './dto/checkout-cart.dto';
 import { JwtAuthGuard } from 'src/auth/auth.guard';
-
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+@ApiTags('Cart')
 @Controller('cart')
 export class CartController {
   constructor(private readonly cartService: CartService) {}
 
   @UseGuards(JwtAuthGuard)
   @Post('add')
+  @ApiOperation({
+    description:
+      'This endpoint allows a user to add an asset to their cart by providing the user ID and asset ID. It returns the cart ID and addition status.',
+    summary: 'Add asset to cart',
+  })
   async addToCart(@Body() createCartItemDto: CreateCartItemDto) {
     return this.cartService.addToCart(createCartItemDto);
   }
 
   @UseGuards(JwtAuthGuard)
   @Get('view')
+  @ApiOperation({
+    description:
+      'This endpoint allows a user to view items in their cart by providing the user ID. It returns a list of cart items and the total price.',
+    summary: 'View cart items',
+  })
   async viewCart(@Query('userId') userId: number) {
     return this.cartService.viewCart(userId);
   }
 
   @UseGuards(JwtAuthGuard)
   @Post('checkout')
+  @ApiOperation({
+    description:
+      'This endpoint allows a user to checkout and purchase the items in their cart by providing the user ID and cart ID. It returns the transaction ID and checkout status.',
+    summary: 'Checkout and purchase',
+  })
   async checkoutCart(@Body() checkoutCartDto: CheckoutCartDto) {
     return this.cartService.checkoutCart(checkoutCartDto);
   }
