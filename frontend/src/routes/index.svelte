279d6cb773101d8ff9f5df9911bffa77dab5e980
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-08-03 21:40:35 +0530

    feat: add greeting when users login

---
+++ frontend/src/routes/index.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  let name: string = 'World';
+
+  function greet() {
+    alert(`Hello, ${name}!`);
+  }
+</script>
+
+<h1>Hello {name}!</h1>
+<button on:click={greet}>Greet</button>
