b6b72e395faa082549c10a453156ea81e800e357
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-08-04 04:22:13 +0530

    feat: add @ApiProperty() to checkout-cart data transfer object (dto)

--- backend/src/cart/dto/checkout-cart.dto.ts
+++ backend/src/cart/dto/checkout-cart.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber } from 'class-validator';
 
 export class CheckoutCartDto {
+  @ApiProperty()
   @IsNumber()
   userId: number;
 
+  @ApiProperty()
   @IsNumber()
   cartId: number;
 }
