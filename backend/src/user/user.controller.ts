97708d063f8c4157d4b9267ad6906f642ad95ba7
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-08-04 04:32:38 +0530

    feat: add API tags, API operations with description, summary at user profile and user logout api endpoints

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -4,19 +4,30 @@ import { UserService } from './user.service';
 import { JwtAuthGuard } from 'src/auth/auth.guard';
 import { UserProfileDto } from './dto/user-profile.dto';
 import { UserLogoutDto } from './dto/user-logout.dto';
-
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+@ApiTags('User')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @UseGuards(JwtAuthGuard)
   @Get('profile')
+  @ApiOperation({
+    description:
+      'This endpoint retrieves the profile information of a user by providing the user ID. It returns the user ID and email.',
+    summary: 'Get user profile',
+  })
   async getUserProfile(@Body() userProfileDto: UserProfileDto) {
     return this.userService.getUserProfile(userProfileDto);
   }
 
   @UseGuards(JwtAuthGuard)
   @Post('logout')
+  @ApiOperation({
+    description:
+      'This endpoint logs out a user by invalidating their authentication token. It requires the user ID and token, and returns the logout status.',
+    summary: 'Logout user',
+  })
   async logoutUser(@Body() userLogoutDto: UserLogoutDto, @Req() req: Request) {
     // Extract token from Authorization header if present
     const authHeader = req.headers['authorization'];
@@ -27,7 +38,6 @@ export class UserController {
       token = authHeader.replace('Bearer ', '');
     } else if (userLogoutDto.token) {
       token = userLogoutDto.token;
-      console.log(token);
     }
 
     if (!token) {
