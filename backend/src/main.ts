66ee75c9b2041362b8e1ae945b35cc2cce2032df
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-08-04 03:24:52 +0530

    feat: initialize swagger using swagger module to set title, description for API documentation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('Real World Asset Marketplace API Endpoints')
+    .setDescription(
+      'API documentation for the Real World Asset Marketplace, a platform for listing, browsing, and purchasing real-world assets using blockchain technology.',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
