46860af0a246ba1bc99287663e7010e35856a753
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-08-04 04:22:26 +0530

    feat: add @ApiProperty() to create-cart data transfer object (dto)

--- backend/src/cart/dto/create-cart.dto.ts
+++ backend/src/cart/dto/create-cart.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber } from 'class-validator';
 
 export class CreateCartItemDto {
+  @ApiProperty()
   @IsNumber()
   userId: number;
 
+  @ApiProperty()
   @IsNumber()
   assetId: number;
 }
