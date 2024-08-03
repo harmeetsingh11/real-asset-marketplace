f73e33d432b27b03f284a7deedba200b929b62bf
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-08-04 04:38:26 +0530

    feat: add @ApiProperty() to connect-wallet data transfer object (dto)

--- backend/src/wallet/dto/connect-wallet.dto.ts
+++ backend/src/wallet/dto/connect-wallet.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsInt, IsString } from 'class-validator';
 
 export class ConnectWalletDto {
+  @ApiProperty()
   @IsInt()
   userId: number;
 
+  @ApiProperty()
   @IsString()
   walletAddress: string;
 }
