b5da3d35d31007e24c302b51a9e0067177826c57
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-08-04 04:03:56 +0530

    feat: add @ApiPropertyOptional() to browse asset data transfer object (dto)

--- backend/src/asset/dto/browse-asset.dto.ts
+++ backend/src/asset/dto/browse-asset.dto.ts
@@ -1,10 +1,13 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsOptional, IsString } from 'class-validator';
 
 export class BrowseAssetsDto {
+  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
   category?: string;
 
+  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
   priceRange?: string;
