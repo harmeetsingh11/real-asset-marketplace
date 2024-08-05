4d825f38395ddff74c5c162be9e2fa5907621e2c
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-08-06 04:00:04 +0530

    chore: update footer text in footer component

--- frontend/src/lib/components/Footer.svelte
+++ frontend/src/lib/components/Footer.svelte
@@ -34,9 +34,7 @@
     </div>
     <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
     <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400"
-      >© 2024 <a href="https://flowbite.com/" class="hover:underline"
-        >RealAssetXchange™</a
-      >. All Rights Reserved.</span
+      >This project was made with ❤️ as a part of TSOC'24 internship</span
     >
   </div>
 </footer>
