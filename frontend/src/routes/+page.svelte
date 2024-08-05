5c6a1937eac2a5bce4866dd0c8fb9335a114466c
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-08-06 03:49:09 +0530

    feat: import footer, hero components to main page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,5 +1,13 @@
 <script>
+  import Footer from '$lib/components/Footer.svelte';
+  import Hero from '$lib/components/Hero.svelte';
   import Navbar from '$lib/components/Navbar.svelte';
 </script>
 
-<Navbar />
+<div class="flex flex-col min-h-screen">
+  <Navbar />
+  <main class="flex-grow flex">
+    <Hero />
+  </main>
+  <Footer />
+</div>
