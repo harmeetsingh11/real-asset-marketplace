c9861cf5477cb08b0a510fb3748f32194e7b6a62
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-08-06 03:41:50 +0530

    feat: implement hero section with Tailwind css

---
+++ frontend/src/lib/components/Hero.svelte
@@ -0,0 +1,88 @@
+<section class="bg-white dark:bg-gray-900 relative flex-grow">
+  <div
+    class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 z-10 relative"
+  >
+    <a
+      href="/"
+      class="inline-flex justify-between items-center py-1 px-1 pe-4 mb-7 text-sm text-primary-700 bg-primary-100 rounded-full dark:bg-primary-900 dark:text-primary-300 hover:bg-primary-200 dark:hover:bg-primary-800"
+    >
+      <span
+        class="text-xs bg-primary-600 rounded-full text-white px-4 py-1.5 me-3"
+        >New</span
+      >
+      <span class="text-sm font-medium"
+        >Our real-world asset marketplace is now live! Explore and invest in
+        real-world assets today.</span
+      >
+      <svg
+        class="w-2.5 h-2.5 ms-2 rtl:rotate-180"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 6 10"
+      >
+        <path
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="m1 9 4-4-4-4"
+        />
+      </svg>
+    </a>
+    <h1
+      class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
+    >
+      Your Gateway to Real-World Assets
+    </h1>
+    <p
+      class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-200"
+    >
+      Explore RealAssetXchange, where cutting-edge technology transforms the way
+      you trade and manage tangible assets, ensuring security, transparency, and
+      efficiency.
+    </p>
+    <form class="w-full max-w-md mx-auto">
+      <label
+        for="default-email"
+        class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+        >Newsletter sign-up</label
+      >
+      <div class="relative">
+        <div
+          class="absolute inset-y-0 rtl:inset-x-0 start-0 flex items-center ps-3.5 pointer-events-none"
+        >
+          <svg
+            class="w-4 h-4 text-gray-500 dark:text-gray-400"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 16"
+          >
+            <path
+              d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z"
+            />
+            <path
+              d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z"
+            />
+          </svg>
+        </div>
+        <input
+          type="email"
+          id="default-email"
+          class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-800 dark:border-gray-700 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+          placeholder="Enter your email here..."
+          required
+        />
+        <button
+          type="submit"
+          class="text-white absolute end-2.5 bottom-2.5 bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+          >Sign up</button
+        >
+      </div>
+    </form>
+  </div>
+  <div
+    class="bg-gradient-to-b from-primary-50 to-transparent dark:from-primary-900 w-full h-full absolute top-0 left-0 z-0"
+  ></div>
+</section>
