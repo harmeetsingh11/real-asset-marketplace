6d676d3a346e469875fa8a8025a042ebf923cddb
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-08-06 03:41:14 +0530

    feat: create footer with tailwindcss

---
+++ frontend/src/lib/components/Footer.svelte
@@ -0,0 +1,42 @@
+<script>
+  import { ChartMixedDollarSolid } from 'flowbite-svelte-icons';
+</script>
+
+<footer class="bg-white shadow dark:bg-gray-900 relative">
+  <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+    <div class="sm:flex sm:items-center sm:justify-between">
+      <a
+        href="/"
+        class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
+      >
+        <ChartMixedDollarSolid class="text-primary-500 w-10 h-10" />
+        <span
+          class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
+          >RealAssetXchange</span
+        >
+      </a>
+      <ul
+        class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400"
+      >
+        <li>
+          <a href="#" class="hover:underline me-4 md:me-6">About</a>
+        </li>
+        <li>
+          <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
+        </li>
+        <li>
+          <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
+        </li>
+        <li>
+          <a href="#" class="hover:underline">Contact</a>
+        </li>
+      </ul>
+    </div>
+    <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+    <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400"
+      >© 2024 <a href="https://flowbite.com/" class="hover:underline"
+        >RealAssetXchange™</a
+      >. All Rights Reserved.</span
+    >
+  </div>
+</footer>
