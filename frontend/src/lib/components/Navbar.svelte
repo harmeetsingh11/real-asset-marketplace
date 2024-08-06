a8b5990d9c02f7eb5d827fa735b7f84074fb2254
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-08-07 04:24:08 +0530

    feat; import the sidebar component in navbar, shows only when user logged in

--- frontend/src/lib/components/Navbar.svelte
+++ frontend/src/lib/components/Navbar.svelte
@@ -1,6 +1,9 @@
 <script>
   import { DarkMode } from 'flowbite-svelte';
   import { ChartMixedDollarSolid } from 'flowbite-svelte-icons';
+  import SidebarComponent from './Sidebar.svelte';
+
+  let loggedIn = true;
 </script>
 
 <nav class="bg-white border-gray-200 dark:bg-gray-900 shadow-lg">
@@ -11,22 +14,29 @@
       <ChartMixedDollarSolid class="text-primary-500 w-10 h-10" />
       <span
         class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
-        >RealAssetXchange</span
       >
+        RealAssetXchange
+      </span>
     </a>
     <div
       class="flex items-center md:order-2 space-x-4 md:space-x-2 rtl:space-x-reverse"
     >
-      <a
-        href="/login"
-        class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-base px-5 py-3 md:px-5 md:py-2.5 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
-        >Login</a
-      >
-      <a
-        href="/register"
-        class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-base px-5 py-3 md:px-5 md:py-2.5 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
-        >Sign up</a
-      >
+      {#if loggedIn}
+        <SidebarComponent /> <!-- Show sidebar when user is logged in -->
+      {:else}
+        <a
+          href="/login"
+          class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-base px-5 py-3 md:px-5 md:py-2.5 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
+        >
+          Login
+        </a>
+        <a
+          href="/register"
+          class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-base px-5 py-3 md:px-5 md:py-2.5 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
+        >
+          Sign up
+        </a>
+      {/if}
       <DarkMode />
     </div>
   </div>
