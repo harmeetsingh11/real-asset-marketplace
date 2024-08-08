0d38dd5d8a9d382bbf892b9718d038fc7f10d8e4
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-08-08 07:05:05 +0530

    feat: add user-account page path to sidebar nav item

--- frontend/src/lib/components/Sidebar.svelte
+++ frontend/src/lib/components/Sidebar.svelte
@@ -91,7 +91,7 @@
             />
           </svelte:fragment>
         </SidebarItem>
-        <SidebarItem label="Account Settings">
+        <SidebarItem label="Account Settings" href="/dashboard/user-account">
           <svelte:fragment slot="icon">
             <UserCircleSolid
               class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
