f5d92c1cf4aa8db07c01509320b21f2a8392899c
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-08-09 22:24:47 +0530

    feat: fetch the logged in state from auth store to protect all the routes from unauthorized user

--- frontend/src/routes/dashboard/+layout.svelte
+++ frontend/src/routes/dashboard/+layout.svelte
@@ -1,6 +1,23 @@
 <script>
   import Footer from '$lib/components/Footer.svelte';
   import Navbar from '$lib/components/Navbar.svelte';
+  import { authStore } from '$lib/stores/authStore';
+  import { onMount } from 'svelte';
+  import { goto } from '$app/navigation';
+  import { redirect } from '@sveltejs/kit';
+
+  let isLoggedIn = false;
+
+  onMount(() => {
+    const unsubscribe = authStore.subscribe((auth) => {
+      isLoggedIn = auth.isLoggedIn;
+      if (!isLoggedIn) {
+        goto('/login'); // Redirect to login if not logged in
+      }
+    });
+
+    unsubscribe();
+  });
 </script>
 
 <Navbar />
