48573c3fb54a0c8d89cf5c9283c2ec74d0546f96
Praneesh-Sharma <pegasus30042004@gmail.com>
2024-08-11 21:05:05 +0530

    added backend/src/wallet docmentation

--- backend/src/wallet/wallet.controller.spec.ts
+++ backend/src/wallet/wallet.controller.spec.ts
@@ -6,15 +6,18 @@ describe('WalletController', () => {
   let controller: WalletController;
 
   beforeEach(async () => {
+    // Set up a testing module with the WalletController and WalletService
     const module: TestingModule = await Test.createTestingModule({
       controllers: [WalletController],
       providers: [WalletService],
     }).compile();
 
+    // Retrieve an instance of WalletController from the testing module
     controller = module.get<WalletController>(WalletController);
   });
 
   it('should be defined', () => {
+    // Assert that the WalletController instance is correctly defined
     expect(controller).toBeDefined();
   });
 });
--- backend/src/wallet/wallet.service.spec.ts
+++ backend/src/wallet/wallet.service.spec.ts
@@ -5,14 +5,17 @@ describe('WalletService', () => {
   let service: WalletService;
 
   beforeEach(async () => {
+    // Set up a testing module with WalletService
     const module: TestingModule = await Test.createTestingModule({
       providers: [WalletService],
     }).compile();
 
+    // Retrieve an instance of WalletService from the testing module
     service = module.get<WalletService>(WalletService);
   });
 
   it('should be defined', () => {
+    // Assert that the WalletService instance is defined
     expect(service).toBeDefined();
   });
 });
