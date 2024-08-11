c62774fe4c872adf9064d028f99e7137c99ddc49
Praneesh-Sharma <pegasus30042004@gmail.com>
2024-08-11 21:18:40 +0530

    added backend/src/auth docs

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -9,6 +9,11 @@ import { DatabaseService } from 'src/database/database.service';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 
+/**
+ * AuthService
+ * 
+ * Handles authentication operations such as login and registration of users.
+ */
 @Injectable()
 export class AuthService {
   constructor(
@@ -16,12 +21,17 @@ export class AuthService {
     private readonly jwtservice: JwtService,
   ) {}
 
+  /**
+   * Authenticates a user based on login data and issues a JWT token.
+   * 
+   * @param loginData - Contains email and password for login.
+   * @returns An object containing the user ID and JWT token.
+   * @throws NotFoundException - If the user is not found or the password is incorrect.
+   */
   async login(loginData: LoginDto) {
     const { email, password } = loginData;
     const user = await this.dataservice.user.findFirst({
-      where: {
-        email: email,
-      },
+      where: { email: email },
     });
     if (!user) {
       throw new NotFoundException('No user exists with the entered email');
@@ -37,11 +47,16 @@ export class AuthService {
     };
   }
 
+  /**
+   * Registers a new user by creating an entry in the database and issuing a JWT token.
+   * 
+   * @param registerData - Contains user details for registration.
+   * @returns An object containing the new user ID and JWT token.
+   * @throws BadGatewayException - If a user with the provided email already exists.
+   */
   async register(registerData: RegisterUserDto) {
     const user = await this.dataservice.user.findFirst({
-      where: {
-        email: registerData.email,
-      },
+      where: { email: registerData.email },
     });
     if (user) {
       throw new BadGatewayException('User with this email already exists');
