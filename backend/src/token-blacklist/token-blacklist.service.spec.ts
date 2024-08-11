8f0730203c2d02d5f8acbb95791c1fccc08bf109
Praneesh-Sharma <pegasus30042004@gmail.com>
2024-08-11 21:09:20 +0530

    added backend/src/token-blacklist docs

--- backend/src/token-blacklist/token-blacklist.service.spec.ts
+++ backend/src/token-blacklist/token-blacklist.service.spec.ts
@@ -5,14 +5,17 @@ describe('TokenBlacklistService', () => {
   let service: TokenBlacklistService;
 
   beforeEach(async () => {
+    // Create a testing module with the TokenBlacklistService
     const module: TestingModule = await Test.createTestingModule({
       providers: [TokenBlacklistService],
     }).compile();
 
+    // Get an instance of TokenBlacklistService from the testing module
     service = module.get<TokenBlacklistService>(TokenBlacklistService);
   });
 
   it('should be defined', () => {
+    // Test that the TokenBlacklistService instance is defined
     expect(service).toBeDefined();
   });
 });
