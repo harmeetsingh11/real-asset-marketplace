a811790e59f0e630c1f834914c99e8fb129533a2
Praneesh-Sharma <pegasus30042004@gmail.com>
2024-08-11 21:10:20 +0530

    added backend/src/database docs

--- backend/src/database/database.service.spec.ts
+++ backend/src/database/database.service.spec.ts
@@ -1,18 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DatabaseService } from './database.service';
 
+/**
+ * Unit tests for the DatabaseService.
+ * Ensures that the DatabaseService is properly defined.
+ */
 describe('DatabaseService', () => {
   let service: DatabaseService;
 
   beforeEach(async () => {
+    // Create a testing module with DatabaseService
     const module: TestingModule = await Test.createTestingModule({
       providers: [DatabaseService],
     }).compile();
 
+    // Retrieve an instance of DatabaseService from the testing module
     service = module.get<DatabaseService>(DatabaseService);
   });
 
   it('should be defined', () => {
+    // Verify that the DatabaseService instance is defined
     expect(service).toBeDefined();
   });
 });
