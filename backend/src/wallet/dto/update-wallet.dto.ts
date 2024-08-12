2f43aaf7a93d8c9ec569339a32c23611b5076ceb
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-08-13 04:13:06 +0530

    chore: remove the unnecessary update-wallet data transfer object (dto)

--- backend/src/wallet/dto/update-wallet.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { ConnectWalletDto } from './connect-wallet.dto';
-
-export class UpdateWalletDto extends PartialType(ConnectWalletDto) {}
