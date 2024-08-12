d4a060685ac1c220127c48f3170232f977748dbe
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-08-12 22:56:09 +0530

    feat: rename the "explore marketplace" button to "Assets history"

--- frontend/src/lib/components/Sidebar.svelte
+++ frontend/src/lib/components/Sidebar.svelte
@@ -85,7 +85,7 @@
             />
           </svelte:fragment>
         </SidebarItem>
-        <SidebarItem label="Explore Marketplace" href="/dashboard/marketplace">
+        <SidebarItem label="Assets History" href="/dashboard/asset-history">
           <svelte:fragment slot="icon">
             <GridSolid
               class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
